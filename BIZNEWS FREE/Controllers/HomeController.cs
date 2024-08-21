7431d6d5335fa219da76b86a147224a239510b4b
HasanovaElgul <98238310+HasanovaElgul@users.noreply.github.com>
2024-08-22 01:08:08 +0400

    fix

--- BIZNEWS FREE/Controllers/HomeController.cs
+++ BIZNEWS FREE/Controllers/HomeController.cs
@@ -1,12 +1,15 @@
-using System.Diagnostics;
+using BIZNEWS_FREE.Data;
 using BIZNEWS_FREE.Models;
+using BIZNEWS_FREE.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace BIZNEWS_FREE.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -15,7 +18,16 @@ namespace BIZNEWS_FREE.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var featuredArticles = _context.Articles
+                .Where(x => x.IsFeature == true && x.IsDeleted == false)
+                .OrderByDescending(x => x.UpdatedDate)
+                .Take(3).ToList();                           //ana sehifede 3 dene olan
+
+            HomeVM homeVM = new()
+            {
+                FeaturedArticles = featuredArticles
+            };
+            return View(homeVM);
         }
 
         public IActionResult Privacy()
---
+++ BIZNEWS FREE/ViewModels/HomeVM.cs
@@ -0,0 +1,9 @@
+﻿using BIZNEWS_FREE.Models;
+
+namespace BIZNEWS_FREE.ViewModels
+{
+    public class HomeVM
+    {
+        public List<Article> FeaturedArticles { get; set; }
+    }
+}
