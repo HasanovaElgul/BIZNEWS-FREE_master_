9e5895cab67d7961bf4bf9a3fa70982f5c63fd5c
HasanovaElgul <98238310+HasanovaElgul@users.noreply.github.com>
2024-08-24 23:33:24 +0400

    fix

--- BIZNEWS FREE/Controllers/ArticleController.cs
+++ BIZNEWS FREE/Controllers/ArticleController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BIZNEWS_FREE.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BIZNEWS_FREE.Controllers
 {
     public class ArticleController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public ArticleController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Detail(int id)
         {
-            return View();
+            var article = _context.Articles
+                .Include(x => x.Category)
+                .Include(x => x.ArticleTags)
+                .Include(x => x.Tags)
+                .FirstOrDefault(x => x.Id == id);
+            return View(article);
         }
 
     }
